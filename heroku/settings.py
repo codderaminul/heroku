a3706e65e0ee23254577ac142d6c891d8243bc89
Md Aminul Haque <113428629+programmeraminul@users.noreply.github.com>
2022-11-01 21:48:39 +0600

    success

--- heroku/settings.py
+++ heroku/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/ 'static'
+STATIC_FILES = os.path.join('staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
